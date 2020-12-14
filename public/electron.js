610d680a1fbdc8afce47ad44aa17ac08289a916b
waqas-ahmd <65waqas@gmail.com>
2020-12-14 14:24:27 +0500

    version-1

--- public/electron.js
+++ public/electron.js
@@ -1,18 +1,22 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const url = require("url");
 const isDev = require("electron-is-dev");
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      devTools: isDev ? true : false,
+    },
+  });
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  //   mainWindow.webContents.openDevTools();
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
